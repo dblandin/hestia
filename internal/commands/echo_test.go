75fd22440a49f096bea39459577a19016757e9e8
Devon Blandin <dblandin@gmail.com>
2018-02-25 16:21:36 -0500

    Add echo command test

---
+++ internal/commands/echo_test.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"github.com/codeclimate/hestia/internal/commands"
+	"github.com/codeclimate/hestia/internal/types"
+	"testing"
+)
+
+type TestNotifier struct {
+	Messages []string
+}
+
+func (n *TestNotifier) Log(message string) {
+	n.Messages = append(n.Messages, message)
+
+}
+
+func TestRun(t *testing.T) {
+	notifier := TestNotifier{}
+
+	command := commands.Echo{User: "test", Input: types.Input{Args: "hello"}, Notifier: &notifier}
+	command.Run()
+
+	messages := notifier.Messages
+	expected := "<@test>: hello"
+
+	if messages[0] != expected {
+		t.Fatalf("Expected `%s`, but received `%s`", expected, messages[0])
+	}
+}
