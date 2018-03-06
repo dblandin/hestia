13c1f1f4a92a9bef503bdff275b5cc8fb08e0b95
Devon Blandin <dblandin@gmail.com>
2018-03-05 23:51:54 -0500

    Alphabetize commands

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -13,16 +13,16 @@ func Build(user string, input types.Input, notifier notifiers.Notifier) Command 
 	var command Command
 
 	switch input.Command {
-	case "whoami":
-		command = WhoAmI{user, input, notifier}
+	case "danceparty":
+		command = DanceParty{user, input, notifier}
 	case "echo":
 		command = Echo{user, input, notifier}
 	case "nowplaying":
 		command = NowPlaying{user, input, notifier}
 	case "weather":
 		command = Weather{user, input, notifier}
-	case "danceparty":
-		command = DanceParty{user, input, notifier}
+	case "whoami":
+		command = WhoAmI{user, input, notifier}
 	default:
 		command = Fallback{user, input, notifier}
 	}
